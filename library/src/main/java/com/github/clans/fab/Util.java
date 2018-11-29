d81e91b04943a320980bb841f0cc48aecb05da86
Xuzan <moonlightsujan@gmail.com>
2018-11-29 23:17:04 +0545

    vector drawable compatibility

--- library/src/main/java/com/github/clans/fab/Util.java
+++ library/src/main/java/com/github/clans/fab/Util.java
@@ -1,6 +1,9 @@
 package com.github.clans.fab;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
 
 final class Util {
@@ -19,5 +22,20 @@ final class Util {
 
     static boolean hasLollipop() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    static Bitmap textAsBitmap(String text, float textSize, int textColor) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.0f); // round
+        int height = (int) (baseline + paint.descent() + 0.0f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
     }
 }
